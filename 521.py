6f89e3991140f3c3910c83531bae1335382f106d
zhaozhuo <2448229422@qq.com>
2020-05-21 17:35:50 +0800

    test

---
+++ 521.py
@@ -0,0 +1,26 @@
+# a = 1
+# def fun(a):
+#     a = 2
+# print(fun(a))
+# print(a)
+
+# a = []
+# def fun(a):
+#     a.append(1)
+# print(fun(a))
+# print(a)
+
+
+a = 1
+
+def fun(a):
+    print (func_in)   # func_in 41322472
+    a = 2
+
+    print(re-point)   # re-point 41322448 41322448
+
+print(func_out)  # func_out 41322472 41322472
+
+fun(a)
+
+print(a) # 1
